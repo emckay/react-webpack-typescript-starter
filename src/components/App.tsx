0fc5b99186939d7173bd5f94c1b657c20b09c403
Eric <eric@opencapital>
2017-12-23 14:40:24 -0700

    fix indentation

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react';
 import './../assets/scss/App.scss';
 
-
 export default class App extends React.Component<{}, undefined> {
   public render() {
     return (
       <div className="app">
-      <h1>Hello World!</h1>
-      <p>Foo to the barz</p>
+        <h1>Hello World!</h1>
+        <p>Foo to the barz</p>
       </div>
     );
   }
